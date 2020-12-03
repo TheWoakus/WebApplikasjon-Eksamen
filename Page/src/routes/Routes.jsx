7f61caa3d21dbd981a35da67d6a1f90e08bd795b
Magnus <magnusmn@hiof.no>
2020-12-03 18:23:36 +0100

    Created empty shell of project and set up basic dependencies

---
+++ Page/src/routes/Routes.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+
+
+const Routes = () => (
+  <Router>
+    // TODO: Add routes
+  </Router>
+);
+
+export default Routes;
---
+++ Page/src/styles/Styled.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import styled from 'styled-components';
+
+//TODO: Add styled components here
