fc281a6e12b47a9b78be32f465f7efd0ef4e00cb
Tommy W. Major <tommywm@hiof.no>
2020-12-06 18:12:34 +0100

    🔥 We can now create articles and view them

---
+++ client/src/utils/articleService.js
@@ -0,0 +1,41 @@
+import http from './http';
+
+const API_URL = '/fagartikler';
+
+export const list = async () => {
+  try {
+    return await http.get(`${API_URL}`);
+  } catch (err) {
+    return err.response.data;
+  }
+};
+
+export const get = async (id) => {
+  try {
+    return await http.get(`${API_URL}/${id}`);
+  } catch (err) {
+    return err.response.data;
+  }
+};
+
+export const create = async (data) => {
+  try {
+    return await http.post(`${API_URL}`, data);
+  } catch (err) {
+    return err.response.data;
+  }
+};
+
+export const patch = async (data) => {
+  try {
+    return await http.patch(`${API_URL}`, data);
+  } catch (err) {
+    return err.response.data;
+  }
+};
+export default {
+  create,
+  list,
+  get,
+  patch,
+};
