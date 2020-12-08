c4aeb621d1a7b06221540bd8c20c9fb38f01add9
Magnus <magnusmn@live.no>
2020-12-08 13:08:26 +0100

    Fixed env issue

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -3,128 +3,147 @@ import axios from 'axios';
 import { withRouter } from 'react-router-dom';
 
 class Form extends React.Component {
+  constructor() {
+    super();
 
-	constructor() {
-		super();
+    this.onChangeName = this.onChangeName.bind(this);
+    this.onChangeMail = this.onChangeMail.bind(this);
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+    this.onChangeVerifyPassword = this.onChangeVerifyPassword.bind(this);
+  }
 
-		this.onChangeName = this.onChangeName.bind(this);
-		this.onChangeMail = this.onChangeMail.bind(this);
-		this.onChangeUsername = this.onChangeUsername.bind(this);
-		this.onChangePassword = this.onChangePassword.bind(this);
-		this.onChangeVerifyPassword = this.onChangeVerifyPassword.bind(this);
-	}
+  onChangeName(event) {
+    this.setState({ name: event.target.value });
+  }
 
-	onChangeName(event) {
-		this.setState({ name: event.target.value })
-	}
+  onChangeMail(event) {
+    this.setState({ mail: event.target.value });
+  }
 
-	onChangeMail(event) {
-		this.setState({ mail: event.target.value })
-	}
+  onChangeUsername(event) {
+    this.setState({ username: event.target.value });
+  }
 
-	onChangeUsername(event) {
-		this.setState({ username: event.target.value })
-	}
+  onChangePassword(event) {
+    this.setState({ password: event.target.value });
+  }
 
-	onChangePassword(event) {
-		this.setState({ password: event.target.value })
-	}
+  onChangeVerifyPassword(event) {
+    this.setState({ verifypassword: event.target.value });
+  }
 
-	onChangeVerifyPassword(event) {
-		this.setState({ verifypassword: event.target.value })
-	}
+  onSubmit(event) {
+    event.preventDefault();
 
-	onSubmit(event) {
-		event.preventDefault()
+    if (this.password.value == this.verifypassword.value) {
+      const userDetails = {
+        name: this.name.value,
+        email: this.mail.value,
+        username: this.username.value,
+        password: this.password.value,
+      };
 
-		if (this.password.value == this.verifypassword.value) {
-			const userDetails = {
-				name: this.name.value,
-				email: this.mail.value,
-				username: this.username.value,
-				password: this.password.value,
-			};
+      axios
+        .post(
+          `${process.env.BASE_URL}${process.env.API_VERSION}/register`,
+          userDetails
+        )
+        .then((res) => {
+          console.log(res.data);
+          this.signupForm.reset();
+          this.props.history.push('/'); // where do we want to send the user after registration??
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      console.log('Passordene stemmer ikke.. prøv igjen.');
+    }
+  }
 
-			axios.post(`${process.env.BASE_URL}${process.env.API_VERSION}/register`, userDetails)
-				.then((res) => {
-					console.log(res.data)
-					this.signupForm.reset();
-					this.props.history.push('/'); // where do we want to send the user after registration??
-				}).catch((error) => {
-					console.log(error)
-				});
-		}
-		else {
-			console.log("Passordene stemmer ikke.. prøv igjen.");
-		}
+  render() {
+    return (
+      <>
+        <form
+          ref={(input) => (this.signupForm = input)}
+          onSubmit={(event) => this.onSubmit(event)}
+        >
+          <fieldset>
+            <label className="registerLabel" htmlFor="name">
+              Navn&#58;
+              <span id="name_error">OBS!! Sjekk at denne er riktig</span>
+            </label>
+            <input
+              ref={(input) => (this.name = input)}
+              type="text"
+              name="name"
+              className="input"
+              placeholder="Vennligst oppgi ditt navn"
+              onChange={this.onChangeName}
+            />
 
-	}
-	render() {
-		return (
-			<>
-				<form
-					ref={(input) => (this.signupForm = input)}
-					onSubmit={(event) => this.onSubmit(event)}
-				>
-					<fieldset>
-						<label className="registerLabel" htmlFor="name">Navn&#58;<span id="name_error">OBS!! Sjekk at denne er riktig</span></label>
-						<input
-							ref={(input) => (this.name = input)}
-							type="text"
-							name="name"
-							className="input"
-							placeholder="Vennligst oppgi ditt navn"
-							onChange={this.onChangeName}
-						/>
+            <label className="registerLabel" htmlFor="mail">
+              Epost&#58;
+              <span id="mail_error">OBS!! Sjekk at denne er riktig</span>
+            </label>
+            <input
+              ref={(input) => (this.mail = input)}
+              type="text"
+              name="mail"
+              className="input"
+              placeholder="Vennligst oppgi din epost"
+              onChange={this.onChangeMail}
+            />
 
-						<label className="registerLabel" htmlFor="mail">Epost&#58;<span id="mail_error">OBS!! Sjekk at denne er riktig</span></label>
-						<input
-							ref={(input) => (this.mail = input)}
-							type="text"
-							name="mail"
-							className="input"
-							placeholder="Vennligst oppgi din epost"
-							onChange={this.onChangeMail}
-						/>
+            <label className="registerLabel" htmlFor="username">
+              Brukernavn&#58;
+              <span id="username_error">OBS!! Sjekk at denne er riktig</span>
+            </label>
+            <input
+              ref={(input) => (this.username = input)}
+              type="text"
+              name="username"
+              className="input"
+              placeholder="Hva ønsker du å ha som ditt brukernavn?"
+              onChange={this.onChangeUsername}
+            />
 
-						<label className="registerLabel" htmlFor="username">Brukernavn&#58;<span id="username_error">OBS!! Sjekk at denne er riktig</span></label>
-						<input
-							ref={(input) => (this.username = input)}
-							type="text"
-							name="username"
-							className="input"
-							placeholder="Hva ønsker du å ha som ditt brukernavn?"
-							onChange={this.onChangeUsername}
-						/>
+            <label className="registerLabel" htmlFor="password">
+              Passord&#58;
+              <span id="password_error">OBS!! Sjekk at denne er riktig</span>
+            </label>
+            <input
+              ref={(input) => (this.password = input)}
+              type="password"
+              name="password"
+              className="input"
+              placeholder="Skriv inn ditt ønskede passord"
+              onChange={this.onChangePassword}
+            />
 
-						<label className="registerLabel" htmlFor="password">Passord&#58;<span id="password_error">OBS!! Sjekk at denne er riktig</span></label>
-						<input
-							ref={(input) => (this.password = input)}
-							type="password"
-							name="password"
-							className="input"
-							placeholder="Skriv inn ditt ønskede passord"
-							onChange={this.onChangePassword}
-						/>
-
-						<label className="registerLabel" htmlFor="verifypassword">Bekreft passord&#58;<span id="verifypassword_error">OBS!! Sjekk at denne er riktig</span></label>
-						<input
-							ref={(input) => (this.verifypassword = input)}
-							type="password"
-							name="verifypassword"
-							className="input"
-							placeholder="Bekreft ditt passord"
-							onChange={this.onChangeVerifyPassword}
-						/>
-						<button
-							type="submit"
-							className="button centered big"
-						>Register</button>
-					</fieldset>
-				</form>
-			</>
-		)
-	}
+            <label className="registerLabel" htmlFor="verifypassword">
+              Bekreft passord&#58;
+              <span id="verifypassword_error">
+                OBS!! Sjekk at denne er riktig
+              </span>
+            </label>
+            <input
+              ref={(input) => (this.verifypassword = input)}
+              type="password"
+              name="verifypassword"
+              className="input"
+              placeholder="Bekreft ditt passord"
+              onChange={this.onChangeVerifyPassword}
+            />
+            <button type="submit" className="button centered big">
+              Register
+            </button>
+          </fieldset>
+        </form>
+      </>
+    );
+  }
 }
 
 export default withRouter(Form);
--- client/src/pages/Fagartikler.jsx
+++ client/src/pages/Fagartikler.jsx
@@ -7,50 +7,57 @@ import PageFooter from '../components/PageFooter';
 import { useAuthContext } from '../context/AuthProvider';
 
 const Fagartikler = () => {
-	const [articles, setArticles] = useState(null);
-	const [error, setError] = useState(null);
-	const {isAdmin, isLoggedIn, setUser} = useAuthContext();
+  const [articles, setArticles] = useState(null);
+  const [error, setError] = useState(null);
+  const { isAdmin, isLoggedIn, setUser } = useAuthContext();
 
-	useEffect(() => {
-		const fetchData = async () => {
-			const { data, error } = await list();
-			if (error) {
-				setError(error);
-			} else {
-				setArticles(data);
-			}
-		};
-		fetchData();
-	}, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await list();
+      if (error) {
+        setError(error);
+      } else {
+        setArticles(data);
+      }
+    };
+    fetchData();
+  }, []);
 
-	return (
-		<>
-			<PageHeader title="Fagartikler" />
-			<section id="page_wrapper">
-				{isAdmin && (
-					<section>
-						<Link to="/nyartikkel/" className="button big bigspace block">Ny artikkel</Link>
-					</section>
-				)}
-				
-				<section id="articles">
-					{articles && articles.map((article) => (
-						<Link className="" to={`/article/${article._id}`} key={article.id}>
-							<section className="article_container">
-								<img className="article_thumbnail" src="" alt="" />
-								<section>
-									<h2 className="articleTitle">{article.title}</h2>
-									<p className="articleCategory">{article.category}</p>
-									<p className="articleIngress">{article.ingress}</p>
-								</section>
-							</section>
-						</Link>
-					))}
-				</section>
-			</section>
-			<PageFooter />
-		</>
-	);
+  return (
+    <>
+      <PageHeader title="Fagartikler" />
+      <section id="page_wrapper">
+        {isAdmin && (
+          <section>
+            <Link to="/nyartikkel/" className="button big bigspace block">
+              Ny artikkel
+            </Link>
+          </section>
+        )}
+
+        <section id="articles">
+          {articles &&
+            articles.map((article) => (
+              <Link
+                className=""
+                to={`/article/${article._id}`}
+                key={article.id}
+              >
+                <section className="article_container">
+                  <img className="article_thumbnail" src="" alt="" />
+                  <section>
+                    <h2 className="articleTitle">{article.title}</h2>
+                    <p className="articleCategory">{article.category}</p>
+                    <p className="articleIngress">{article.ingress}</p>
+                  </section>
+                </section>
+              </Link>
+            ))}
+        </section>
+      </section>
+      <PageFooter />
+    </>
+  );
 };
 
 export default Fagartikler;
