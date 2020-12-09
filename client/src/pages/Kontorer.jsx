eb18f87738aed9100c1d225ca91158b4bba8073d
Tommy W. Major <tommywm@hiof.no>
2020-12-09 13:17:16 +0100

    pushing offices to db with the click of a button

--- client/src/pages/Kontorer.jsx
+++ client/src/pages/Kontorer.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import axios from 'axios';
 
-import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 import PageHeader from '../components/PageHeader.jsx';
@@ -9,69 +8,27 @@ import PageFooter from '../components/PageFooter.jsx';
 import Search from '../components/Search.jsx';
 import FetchOffices from '../components/FetchOffices.jsx';
 
-import { DBOffices } from '../assets/js/db_offices.json';
-
-const Section = styled.section`
-  border: 1px solid black;
-  padding: 20px;
-`;
-
-const Title = styled.p`
-  font-size: 16px;
-  font-weight: bold;
-  margin-top: 0;
-  margin-bottom: 5px;
-  grid-area: list_title;
-`;
-
-const Address = styled.p`
-  margin: 0;
-  grid-area: list_address;
-`;
-
-const Phone = styled.p`
-  margin: 0;
-  grid-area: list_phone;
-`;
-
-const Mail = styled.a`
-  margin: 0;
-  grid-area: list_mail;
-`;
+import { dbOffices } from '../assets/js/dbOffices.js';
 
 class Kontorer extends React.Component {
   constructor() {
     super();
 
-    this.loadSamples = this.loadSamples.bind(this);
     this.populateDB = this.populateDB.bind(this);
     this.search = this.search.bind(this);
 
     this.state = {
       search: null,
       viewType: true,
-      db_offices: {},
+      update: '',
     };
   }
 
-  componentDidMount() {
-    this.loadSamples();
-    // this.populateDB();
-  }
-
-  loadSamples() {
-    this.setState({
-      db_offices,
-    });
-  }
-
   populateDB() {
-    const collection = this.state.db_offices;
-    console.log(collection);
     axios
       .post(
         `${process.env.BASE_URL}${process.env.API_VERSION}/kontorer`,
-        collection
+        dbOffices
       )
       .then((res) => {
         console.log(res.data);
@@ -81,6 +38,7 @@ class Kontorer extends React.Component {
       .catch((error) => {
         console.log(error);
       });
+    this.setState({ update: '1' });
   }
 
   search(keyword) {
@@ -93,49 +51,18 @@ class Kontorer extends React.Component {
 
   render() {
     const viewtype = this.state.viewType ? 'gridOffices' : 'listOffices';
-    const currentlocation = 'start';
-    const counter = 1;
-
-    // const allOfficeLocations = Object.values(this.state.allLocations)
-    //   .filter((data) => {
-    //     if (this.state.search == null) {
-    //       return data;
-    //     }
-    //     if (
-    //       data.title.toLowerCase().includes(this.state.search.toLowerCase()) ||
-    //       data.address
-    //         .toLowerCase()
-    //         .includes(this.state.search.toLowerCase()) ||
-    //       data.phone.toLowerCase().includes(this.state.search.toLowerCase()) ||
-    //       data.location.toLowerCase().includes(this.state.search.toLowerCase())
-    //     ) {
-    //       return data;
-    //     }
-    //   })
-    //   .map((data) => (
-    //     <Section className="oneOffice" key={`${data.location}${data.id}`}>
-    //       <Title className="list_title">{data.title}</Title>
-    //       <Address className="list_address">
-    //         {data.address} {data.id}
-    //       </Address>
-    //       <Phone className="list_phone">
-    //         {data.phone}
-    //         {data.id}
-    //       </Phone>
-    //       <Mail
-    //         className="list_mail"
-    //         href={`mailto:${data.location}${data.id}@epost.no`}
-    //       >
-    //         {data.location}
-    //         {data.id}@epost.no
-    //       </Mail>
-    //     </Section>
-    //   ));
 
     return (
       <>
         <PageHeader title="Våre kontorer" />
         <section id="page_wrapper">
+          <button
+            type="submit"
+            onClick={this.populateDB}
+            className="button big block"
+          >
+            Generer kontorer
+          </button>
           <section>
             <Link to="/nyttkontor/" className="button big block">
               Nytt kontor
